b330c4600136c94e58c0e6d62b34b9d90db64596
aryakarthi <aryaperumal.m@gmail.com>
2024-10-07 17:21:53 +0530

    updated api base url

--- client/src/api/baseAPI.js
+++ client/src/api/baseAPI.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
   // baseUrl: "http://localhost:3000",
-  baseUrl: import.meta.VITE_API_BASE_URL,
+  baseUrl: import.meta.env.VITE_API_BASE_URL,
   credentials: "include",
 });
 
