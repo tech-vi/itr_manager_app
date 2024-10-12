b80d0447d4a2f46f912fcf2d82622b4532430624
aryakarthi <aryaperumal.m@gmail.com>
2024-10-12 14:31:58 +0530

    tested with utility functions for local storage

--- client/src/app/features/authSlice.js
+++ client/src/app/features/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+// import { setLocalStorageData } from "../../utils/helper.js";
 
 const user = JSON.parse(localStorage.getItem("vi")) || null;
 
@@ -15,6 +16,7 @@ export const userAuthSlice = createSlice({
       state.user = action.payload;
       state.isLoggedIn = true;
       localStorage.setItem("vi", JSON.stringify(action.payload));
+      // setLocalStorageData("vi", action.payload, 3);
     },
     logout: (state) => {
       state.user = null;
