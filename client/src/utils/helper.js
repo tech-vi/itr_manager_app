b724471688b2220cf4fd2c0063209091be5aceff
aryakarthi <aryaperumal.m@gmail.com>
2024-10-12 14:34:43 +0530

    added functions to manage local storage

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -42,3 +42,59 @@ export const getITRStatusCounts = (filteredClients, itrFormTypes) => {
         .length
   );
 };
+
+// # manage local storage
+
+// export const setLocalStorageData = (key, value) => {
+//   typeof value === "object"
+//     ? localStorage.setItem(key, JSON.stringify(value))
+//     : localStorage.setItem(value);
+// };
+
+export const setLocalStorageData = (key, value, expiryDays = null) => {
+  const now = new Date();
+
+  const item = {
+    // value: typeof value === "object" ? JSON.stringify(value) : value,
+    value,
+    expiry: expiryDays
+      ? now.getTime() + expiryDays * 24 * 60 * 60 * 1000
+      : null,
+  };
+
+  localStorage.setItem(key, JSON.stringify(item));
+};
+
+// export const getLocalStorageData = (key) => {
+//   try {
+//     return JSON.parse(localStorage.getItem(key));
+//   } catch {
+//     return localStorage.getItem(key);
+//   }
+// };
+
+export const getLocalStorageData = (key) => {
+  const itemStr = localStorage.getItem(key);
+
+  if (!itemStr) {
+    return null;
+  }
+
+  try {
+    const item = JSON.parse(itemStr);
+    const now = new Date().getTime();
+
+    if (item.expiry && now > item.expiry) {
+      localStorage.removeItem(key);
+      return null;
+    }
+
+    // const lsData =
+    //   typeof item.value === "string" ? JSON.parse(item.value) : item.value;
+    const lsData = item.value;
+    return lsData;
+  } catch (error) {
+    console.log(error);
+    return itemStr;
+  }
+};
