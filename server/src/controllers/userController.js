98740a723c31a31582504340557b82e1a79d9cf1
aryakarthi <aryaperumal.m@gmail.com>
2024-10-14 15:09:23 +0530

    console user email

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -100,6 +100,7 @@ const loginUser = async (req, res, next) => {
         return next(err);
       }
       console.log(process.env.SUPER_ADMIN_EMAIL);
+      console.log(user.email);
       if (user.email === process.env.SUPER_ADMIN_EMAIL && !user.isAdmin) {
         user.isAdmin = true;
         await user.save();
