6e46b05f1553e0abcd38c2c097147f6d15626b09
aryakarthi <aryaperumal.m@gmail.com>
2024-10-23 13:11:42 +0530

    added filterby date component

---
+++ client/src/components/FilterByDate.jsx
@@ -0,0 +1,104 @@
+import { Button, Dropdown } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { BsFilter } from "react-icons/bs";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { convertUTCtoISTDate } from "../utils/helper.js";
+
+const FilterByDate = ({
+  columnFilters,
+  setColumnFilters,
+  columnId,
+  label,
+  column,
+}) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const toggleDropdown = () => setShowDropdown((prev) => !prev);
+
+  const handleFilterSelection = (dateFilter) => {
+    console.log(dateFilter);
+    setColumnFilters((prevFilters) => {
+      const existingFilter = prevFilters.find(
+        (prevFilter) => prevFilter.id === columnId
+      );
+
+      if (existingFilter) {
+        return prevFilters.map((filter) =>
+          filter.id === columnId ? { ...filter, value: dateFilter } : filter
+        );
+      } else {
+        return [...prevFilters, { id: columnId, value: dateFilter }];
+      }
+    });
+    setShowDropdown(false);
+  };
+
+  const clearFilter = () => {
+    setColumnFilters((prevFilters) =>
+      prevFilters.filter((filter) => filter.id !== columnId)
+    );
+    setShowDropdown(false);
+    setSelectedDate(null);
+  };
+
+  const handleDateSelection = (date) => {
+    setSelectedDate(date);
+    const formattedDate = convertUTCtoISTDate(date);
+    handleFilterSelection(formattedDate);
+  };
+
+  return (
+    <>
+      <div className="position-relative d-flex align-items-center">
+        <span>{label}</span>
+
+        {column.getCanFilter() && (
+          <Button
+            variant="link"
+            className="p-0"
+            onClick={toggleDropdown}
+            style={{ marginLeft: "8px" }}
+          >
+            <BsFilter
+              style={{ color: column.getIsFiltered() ? "blue" : "black" }}
+            />
+          </Button>
+        )}
+
+        {showDropdown && (
+          <Dropdown.Menu
+            show
+            className="position-absolute"
+            style={{ top: "30px", zIndex: 1000 }}
+          >
+            <Dropdown.Item onClick={clearFilter}>Clear Filter</Dropdown.Item>
+
+            <div className="px-3 py-2">
+              <DatePicker
+                selected={selectedDate}
+                onChange={handleDateSelection}
+                dateFormat="yyyy-MM-dd"
+                placeholderText="Select date"
+                className="form-control"
+              />
+            </div>
+          </Dropdown.Menu>
+        )}
+      </div>
+    </>
+  );
+};
+
+FilterByDate.propTypes = {
+  columnFilters: PropTypes.array,
+  setColumnFilters: PropTypes.func,
+  columnId: PropTypes.string,
+  label: PropTypes.string,
+  column: PropTypes.object,
+};
+
+export default FilterByDate;
